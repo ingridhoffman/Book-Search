d31905d6f1cc6f4acd21bf5052f8ddf7dd9c5d8d
trapezechic <ingridhoffman@me.com>
2020-06-10 21:20:28 -0700

    build app framework

--- client/src/components/App/app.js
+++ client/src/components/App/app.js
@@ -1,12 +1,35 @@
 // Global
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Local
 import "./app.css";
+import Navbar from "../Navbar";
+import Home from "../../pages/home";
+import MyBooks from "../../pages/mybooks";
+import Oops from "../../pages/oops";
 
 // Export
 function App() {
-	return <div></div>;
+	return (
+		<div className="container">
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path={["/", "/Book-Search"]}>
+						<Home />
+					</Route>
+					<Route path="/mybooks">
+						<MyBooks />
+					</Route>
+					<Route>
+						<Oops />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
